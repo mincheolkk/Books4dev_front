bb0302dd7cc9ed9556898c6e03c1f6201e9ab952
mincheolkk <kmc04080408@gmail.com>
2022-07-24 21:28:44 +0900

    feat : 위시리스트 추가

---
+++ src/components/AddWish.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn 
+        text
+        x-large
+        @click="wishBook()"
+        ><p> + 관심있는 책
+        </p></v-btn>
+</template>
+
+<script>
+import ApiService from "../api";
+
+export default {
+    props:['isbn','title','thumbnail'],
+    
+    methods:{
+        wishBook(){
+            const request = {
+                isbn: this.isbn,
+                title: this.title,
+                thumbnail: this.thumbnail
+            };
+            console.log(request);
+            // this.$store.dispatch("saveWishList",request);
+            ApiService.post(`http://localhost:8080/book/wish`, request);
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
