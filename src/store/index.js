b3acfb11e4ab1b76a003500ee4f3b6a70c8ada83
mincheolkk <kmc04080408@gmail.com>
2022-07-28 12:44:08 +0900

    accessToken을 각 메서드 안에서 받게 설정

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import book from "./modules/book"
+import login from "./modules/login"
+
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+    modules: {
+        book,
+        login
+    }
+});
