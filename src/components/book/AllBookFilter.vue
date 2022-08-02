9b78a9a3f9bf0a01d8510d4e482c43a25e8a620e
mincheolkk <kmc04080408@gmail.com>
2022-08-03 01:05:04 +0900

    feat : 전체 책 필터링

---
+++ src/components/book/AllBookFilter.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="filter-box">
+    <br />
+    <v-select
+      class="filters"
+
+      v-model="memberType"
+      v-on:change="onChange"
+      item-text="text"
+      item-value="key"
+      :items=memberTypeMap
+      hide-details
+      placeholder="등록한 개발자"
+      menu-props="auto"
+      single-line
+    ></v-select>
+    <v-select
+      class="filters"
+
+      v-model="sortType"
+      v-on:change="onChange"
+
+      item-text="text"
+      item-value="key"
+      :items=sortTypeMap
+      menu-props="auto"
+      placeholder="조회 순서"
+      hide-details
+      single-line
+    ></v-select>
+    <v-select
+      class="filters"
+
+      v-model="recommendType"
+      v-on:change="onChange"
+
+      item-text="text"
+      item-value="key"
+      :items=recommendTypeMap
+      menu-props="auto"
+      placeholder="추천 시기"
+      hide-details
+      single-line
+    ></v-select>
+    <v-btn
+            color="blue darken-1"
+            text
+            @click="goFilter()"
+          >
+            조회
+          </v-btn>
+
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+        memberTypeMap: [
+            { key: "", text: "모든 개발자" },
+            { key: "BACK", text: "백엔드 개발자" },
+            { key: "FRONT", text: "프론트 개발자" }            
+        ],
+        
+        sortTypeMap: [
+            { key: "COUNT", text: "등록 많은 순" },
+            { key: "STAR", text: "평점 높은 순" }
+        ],
+        
+        recommendTypeMap: [
+            { key: "", text: "모든 시기" },
+            { key: "before", text: "취업 전" },
+            { key: "after", text: "취업 후 ~ 2년" },
+            { key: "twoYear", text: "취업 후 2년 ~ 5년" },
+            { key: "fiveYear", text: "취업 후 5년 ~ 10년" },
+            { key: "tenYear", text: "취업 후 10년 ~ "}
+        ],
+
+        memberType: "",
+        sortType:"",
+        recommendType:"",
+        tempUrl:"",
+    };
+  },
+
+  methods:{
+        async allBooks() {
+          this.books =  await this.$store.dispatch("fetchAllBooks")
+        },
+      
+        async createFilterUrl () {
+          const filterUrl = "?"+"memberType="+this.memberType+
+                        "&"+"sortType="+this.sortType+
+                        "&"+"recommendType="+this.recommendType;
+          return filterUrl;
+        },
+       
+       async onChange() {
+        this.tempUrl = await this.createFilterUrl();
+        // await this.$router.push(filterUrl);
+        console.log("filterurl" + this.tempUrl);
+        this.$store.dispatch("fetchAllBooks" );
+    }
+  }
+}
+</script>
+
+<style scoped>
+.filter-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Noto Sans KR", "Noto Sans JP", sans-serif;
+}
+
+.filters {
+  padding: 0;
+  width: 10em;
+  max-width: 12em;
+  margin-right: 50px;
+   box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  
+  
+}
+
+.filters:last-child {
+  margin-right: 0;
+}
+
+.filter-box.mobile {
+  margin-top: 30px;
+  justify-content: center;
+  margin-right: 0;
+}
+
+.filters.mobile {
+  max-width: 100px;
+  margin: 0 10px 15px 0;
+}
+</style>
