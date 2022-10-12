d4331a958336083d1939f91fbea16abf778bf967
mincheolkk <kmc04080408@gmail.com>
2022-10-12 23:05:58 +0900

    refactor : book.js 수정

--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -2,19 +2,30 @@ import ApiService from "@/"
 
 export default {
     state: {
-        resultLists: [],
+        resultList: [],
         searchList: [],
+        registedList: [],
         kakaoData: "",
+        readBook: {},
+        wishBookList: [],
     },
 
     getters: {
         getResultList: (state) => {
             return state.resultList;
-        }
-        ,
+        },
         getSearchList: (state) => {
             return state.searchList;
-        }
+        },
+        getRegistedList: (state) => {
+            return state.registedList;
+        },
+        getReadBook: (state) => {
+            return state.readBook;
+        },
+        getWishBook: (state) => {
+            return state.wishBookList;
+        },
     },
 
     mutations: {
@@ -23,31 +34,68 @@ export default {
         },
         setSearchList(state, searchList) {
             state.searchList = searchList;
-        }
+        },
+        setRegistedList(state, searchRegistedList) {
+            state.registedList = searchRegistedList;
+        },
+        setReadBook(state, res) {
+            state.readBook = res;
+        },
+        setWishBook(state, res) {
+            state.wishBookList = res;
+        },
     },
 
     actions: {
-        async fetchAllBooks(){
+        async fetchAllBooks({commit}){
             const res = await ApiService.get('http://localhost:8081/test/all');
-            console.log(res);
-            const resultList = res.data.resultList;
-            return resultList;
+            const resultBook = res.data.body;
+            commit("setResultList", resultBook)
+            return resultBook;
         },
+        
         async searchBook ({commit}, param) {
+            // 등록된 책 가져오기
+            const resSec  = await ApiService.get(`http://localhost:8081/search/readbook?query=${param}`);
+            const registeredList = resSec.data;
+            commit("setRegistedList", registeredList);
+            
+            const isbnList = [];
+            for (let i = 0; i < registeredList.length; i++) {
+                isbnList.push(registeredList[i].isbn);
+                console.log("store isbnList = " + isbnList);
+            }
+      
+            // 책 검색 가져오기
             const res = await ApiService.get(`http://localhost:8081/todo?query=${param}`);
             const searchBook = res.data.documents;
-            commit("setSearchList",searchBook);
-            return searchBook;
+      
+            // 중복 제거
+            for (let i=searchBook.length-1; i>=0; i--) {
+                if(isbnList.includes(searchBook[i].isbn)){
+                    searchBook.splice(i,1);
+                }
+            }
+            commit("setSearchList",searchBook);            
         },
         async saveWishList(request) {
-
-             ApiService.post(`http://localhost:8081/book/wish`, request);
+            ApiService.post(`http://localhost:8081/book/wish`, request);
         },
-        async filterAllBooks({commit}, param){
+      
+        async filterAllBooks({commit}, param) {
             const res = await ApiService.get(`http://localhost:8081/test/all${param}`);
             const resultBook = res.data.body;
             commit("setResultList", resultBook)
             return resultBook;
+        },
+        async fetchReadBook({ commit }) {
+            const fetchData = await ApiService.getWithToken("http://localhost:8081/test/readBook");
+            commit("setReadBook", fetchData.data);
+        },
+      
+        async fetchWishBook({ commit }) {
+            const fetchData = await ApiService.getWithToken("http://localhost:8081/test/wish");
+            commit("setWishBook", fetchData.data);
         },
     } 
 }
