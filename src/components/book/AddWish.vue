fc5b55d041716e2a7603bed6f94560c58836455e
mincheolkk <kmc04080408@gmail.com>
2023-02-18 21:27:31 +0900

    feat : wishbook 등록시  error 처리 추가

--- src/components/book/AddWish.vue
+++ src/components/book/AddWish.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-btn 
-        text
-        x-large
-        @click="wishBook() "
-        ><p> + 관심있는 책
-        </p></v-btn>
-</template>
+        <v-btn
+          class="wish-style"
+          color="#FFFFFF"
+          :style="{'font-size': '17px'}"
+          depressed
+
+          @click="wishBook()"
+        >
+         + 관심있는 책
+        </v-btn>
+      </template>
 
 <script>
 import ApiService from "../../index";
@@ -15,39 +19,51 @@ export default {
     props:['isbn','title','thumbnail'],
     computed: {
       ...mapGetters([
-        "isLoggedIn"
+        "isLoggedIn", 
       ])
     },
     methods:{
-        wishBook(){
+        async wishBook(){
             if (!this.isLoggedIn) {
-              alert("로그인 부탁드립니다 :)")
-              location.reload();
-              return
+              await this.$store.dispatch(
+                "updateSnackbarText",
+                "로그인 후 이용해주세요."
+              );
+
+              await this.$store.dispatch(
+                "closeSnackbarAfterTimeout",
+                1000
+              );
             }
+
             const request = {
                 isbn: this.isbn,
                 title: this.title,
                 thumbnail: this.thumbnail
             };
-            // this.$store.dispatch("saveWishList",request);
-            ApiService.postWithToken(`/book/wish`, request);
-            alert("굳 :)")
 
-              return
+            
+            ApiService.postWithToken(`http://localhost:8084/book/wish`, request)
+              .then(()=> {
+                  this.$store.dispatch("updateSnackbarText","등록되었습니다.")
+                  .then(() => 
+                    this.$store.dispatch("closeSnackbarAfterTimeout",1000)
+                  )
+              })
+              .catch(error => {
+                if (error.response.status === 406){
+                  this.$store.dispatch("updateSnackbarText", "이미 등록되었습니다.");
+                  this.$store.dispatch("closeSnackbarAfterTimeout", 1000);
+                }
+              });
             },
         },
-        checkLogin() {
-            if (!this.isLoggedIn) {
-              alert("로그인 부탁드립니다 :)")
-              location.reload();
-              return
-        }
-        }
     }
 
 </script>
 
 <style>
-
+.wish-style {
+  margin-bottom: 5px;
+}
 </style>
