faedcc904e7e18152c62a336ed32a0ba99a5ad09
mincheolkk <kmc04080408@gmail.com>
2023-02-25 01:10:49 +0900

    router 추가및 수정

--- src/router.js
+++ src/router.js
@@ -1,17 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import HomeHeader from "./components/HomeHeader";
-// import BookInput from "./components/BookInput";
 import BookList from "./components/book/BookList";
 import SearchList from './components/book/SearchList';
 import SelectPosition from './components/login/SelectPosition'
 import NewLogin from './components/login/NewLogin'
-// import App from './App.vue'
-// import MyPage from './components/mypage/MyPage'
 import MyWishBook from './components/mypage/MyWishBook'
 import MyReadBook from './components/mypage/MyReadBook'
 import ChangePosition from './components/mypage/ChangePosition'
-
+import LoginPage from './components/login/LoginPage'
+import DetailBook from './components/book/DetailBook'
 
 Vue.use(VueRouter);
 
@@ -19,21 +16,20 @@ export const router = new VueRouter({
     mode:'history',
     base: process.env.BASE_URL,
     routes:[
-        // { path: '/', component: App},
-        // { path: '/home', component: HomeHeader},
-        // { path: '/home', component: BookInput},
         { path: '/', component: BookList},
         { path: "/search", component: SearchList},
         { path: "/selectPosition", component: SelectPosition},
         { path: "/init", component:NewLogin},
-        { path: "/mypage", 
-          component:MyReadBook,
-        },
-        { path: "/mywish", component:MyWishBook},
-        { path: "/changePosition", component: ChangePosition}
-        // { path: "/myread", component:My}
 
-    ]
+        { path: "/mypage", component:MyReadBook,},
+        { path: "/mywish", component:MyWishBook},
+        { path: "/changePosition", component: ChangePosition},
+        { path: "/login", component: LoginPage},
+        { path: "/books/:id", component: DetailBook},
+    ],
+    scrollBehavior() {
+      return { top: 0}
+    },
 
 })
 
