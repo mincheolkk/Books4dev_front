e1a5dd9e8708d42457e148e9134c025a7ee34672
mincheolkk <kmc04080408@gmail.com>
2023-02-27 02:10:32 +0900

    feat : 앱 바깥부분 설정

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,55 @@
 <template>
+<div class="outer-app">
   <v-app id="app">
     <snack-bar></snack-bar>
     <home-header />
-    <!-- <book-list /> -->
     <router-view />
+    <home-footer></home-footer>
   </v-app>
+</div>
 </template>
 
 <script>
-// import BookList from './components/BookList.vue'
 import HomeHeader from './components/HomeHeader.vue'
 import SnackBar from "./components/SnackBar";
+import HomeFooter from "./components/HomeFooter";
+
 
 export default {
   name: 'App',
   components: {
     HomeHeader,
-    // BookList,
-    SnackBar
+    SnackBar,
+    HomeFooter,
   },
-  created() {
-    // this.$router.push()
-  }
+
 }
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.outer-app {
+  margin: 0;
+  width: 100%;
+  min-height: 100%;
+  position: relative;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+
+  background-color: #172036;
+}
 #app { 
-  margin:0 auto; 
-  width:60%; 
+  margin: 0 auto;
+  width:768px; 
   min-height:100%; 
-  padding-top:20px; 
-  border-left:1px #D8D8D8 solid; 
-  border-right:1px #D8D8D8 solid; 
   position:relative; 
   text-align:center;
-  box-shadow: cm;
 }
-/* 여백제거 + 드래그방지 */
-  /* body{ padding:0; margin:0; -ms-user-select: none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; user-select: none; } */
 
-  /* 반응형 시작 */
-  @media(max-width:750px){
-    #app{ width:100%; border-left:0; border-right:0; }
+@media(max-width:768px){
+  #app{ 
+      width:100%; 
+      margin: 0 auto;
+      border-left:0; 
+      border-right:0; }
   }
 
 </style>
