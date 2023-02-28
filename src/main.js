0fa8f4bfbde331e33b3c3a480ffd1eafc81aaaf3
mincheolkk <kmc04080408@gmail.com>
2023-03-01 00:44:37 +0900

    refactor : main.js 에서 에러 핸들링

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { router } from './router.js'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import VueMq from "vue-mq";
+import InfiniteLoading from 'vue-infinite-loading';
+
 
 
 Vue.use(Vuetify);
@@ -21,10 +23,17 @@ Vue.use(VueMq, {
 
 Vue.config.productionTip = false
 Vue.config.errorHandler = function(err) {
-  if (err.response || err.response.status === 429) {
+  if (err.response && err.response.status === 429) {
     alert('너무 많은 요청을 보냈습니다. 잠시 후 다시 시도해보세요');
   }
+  if (err.response && err.response.status === 401) {
+    alert('로그인이 필요합니다');
+    this.$router.push('/');
+  }
 };
+
+Vue.component('infinite-loading', InfiniteLoading);
+
 new Vue({
   store,
   router,
