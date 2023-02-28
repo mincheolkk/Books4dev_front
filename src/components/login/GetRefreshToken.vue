0fcb2035a5410020b5a4fc1d196fd5b484027ff5
mincheolkk <kmc04080408@gmail.com>
2023-03-01 00:15:56 +0900

    임시 컴포넌트 명에서 정식이름으로 변경

---
+++ src/components/login/GetRefreshToken.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      잠시만 기다려주세요.
+  </div>
+</template>
+
+<script>
+import ApiService from "../../index";
+
+export default {
+   
+    async beforeCreate() {
+        const token = this.$route.query.token;
+        const obj = {
+            "accessToken":token,
+            expire:Date.now() + 1000 * 60 * 60
+        }
+        localStorage.setItem("accessToken",JSON.stringify(obj));
+        
+        const refresh = await ApiService.getWithToken("http://localhost:8084/auth/refreshtoken");
+        localStorage.setItem("refreshToken", refresh.data);
+
+        this.$router.push('/member/selectPosition');
+    },
+}
+</script>
+
+<style>
+
+</style>
